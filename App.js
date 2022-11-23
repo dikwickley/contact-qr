fede123bced371fefeb999646ec860f07c2d3703
aniket singh rawat <aniket.rawat@bytelearn.ai>
2022-11-23 22:42:36 +0530

    initial commit and setup

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+import { HomeScreen } from "./screens/HomeScreen";
+import { ScannerScreen } from "./screens/ScannerScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name={"home"} size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Scanner"
+          component={ScannerScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name={"camera"} size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
